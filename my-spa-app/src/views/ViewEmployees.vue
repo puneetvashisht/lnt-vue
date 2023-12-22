bc6ddb0a63d419ca90716713f7efbf3997087bab
Puneet Vashisht <puneetvashisht@gmail.com>
2023-12-22 15:53:23 +0530

    single page application

---
+++ my-spa-app/src/views/ViewEmployees.vue
@@ -0,0 +1,46 @@
+<template lang="">
+    <!-- <button @click="loadData">Load</button> -->
+   <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Salary</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="employee in employees" :key="employee.id">
+      <th scope="row">{{employee.id}}</th>
+      <td>{{employee.name}}</td>
+      <td>{{employee.salary}}</td>
+      <td>@mdo</td>
+    </tr>
+    
+  </tbody>
+</table>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            employees: []
+        }
+    },
+
+    methods: {
+        loadData(){
+            fetch('http://localhost:3000/employees')
+            .then(res=>res.json())
+            .then(data=> this.employees = data)
+        }
+    },
+    mounted() {
+        console.log('After component initialized... ');
+        this.loadData();
+    },
+}
+</script>
+<style lang="">
+    
+</style>
